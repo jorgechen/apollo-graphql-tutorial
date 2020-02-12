fcf276713075d3e77216d1ddcd7bbbed14bf7bc2
George C <george.chen@arundo.com>
2020-02-12 11:07:16 -0600

    more resolvers

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -2,6 +2,36 @@ const { paginateResults } = require('./utils')
 
 module.exports = {
   Query: {
+    Mission: {
+      // make sure the default size is 'large' in case user doesn't specify
+      missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+        return size === 'SMALL'
+          ? mission.missionPatchSmall
+          : mission.missionPatchLarge
+      },
+    },
+
+    Launch: {
+      isBooked: async (launch, _, { dataSources }) =>
+        dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+    },
+    
+    User: {
+      trips: async (_, __, { dataSources }) => {
+        // get ids of launches by user
+        const launchIds = await dataSources.userAPI.getLaunchIdsByUser()
+
+        if (!launchIds.length) return []
+
+        // look up those launches by their ids
+        return (
+          dataSources.launchAPI.getLaunchesByIds({
+            launchIds,
+          }) || []
+        )
+      },
+    },
+
     launches: async (_, { pageSize = 20, after }, { dataSources }) => {
       const allLaunches = await dataSources.launchAPI.getAllLaunches()
       // we want these in reverse chronological order
