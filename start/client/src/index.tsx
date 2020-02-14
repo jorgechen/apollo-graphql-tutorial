4c07ff74ef96b5e942bc3372fd0b5defaf48cc8b
George C <george.chen@arundo.com>
2020-02-13 19:02:55 -0600

    client index.tsx

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,7 +1,11 @@
-import gql from 'graphql-tag'
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { ApolloProvider } from '@apollo/react-hooks'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Pages from './pages'
+import injectStyles from './styles'
 
 const cache = new InMemoryCache()
 const link = new HttpLink({
@@ -13,18 +17,10 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   link,
 })
 
-// ... above is the instantiation of the client object.
-client
-.query({
-  query: gql`
-    query GetLaunch {
-      launch(id: 56) {
-        id
-        mission {
-          name
-        }
-      }
-    }
-  `,
-})
-.then(result => console.log(result))
+injectStyles()
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root'),
+)
