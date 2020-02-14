432ed21890ee58d697f294c47d690ccf02b946a0
George C <george.chen@arundo.com>
2020-02-14 12:51:23 -0600

    launches.tsx

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,10 +1,10 @@
-import React, { Fragment } from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
+import React, { Fragment } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
-import { LaunchTile, Header, Button, Loading } from '../components';
-import { RouteComponentProps } from '@reach/router';
-import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+import { LaunchTile, Header, Button, Loading } from '../components'
+import { RouteComponentProps } from '@reach/router'
+import * as GetLaunchListTypes from './__generated__/GetLaunchList'
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -20,36 +20,44 @@ export const LAUNCH_TILE_DATA = gql`
       missionPatch
     }
   }
-`;
+`
 
-export const GET_LAUNCHES = gql`
-  query GetLaunchList($after: String) {
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
     launches(after: $after) {
       cursor
       hasMore
       launches {
-        ...LaunchTile
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
       }
     }
   }
-  ${LAUNCH_TILE_DATA}
-`;
+`
 
-interface LaunchesProps extends RouteComponentProps {}
+interface LaunchesProps extends RouteComponentProps {
+}
 
 const Launches: React.FC<LaunchesProps> = () => {
   const {
     data,
     loading,
     error,
-    fetchMore
   } = useQuery<
     GetLaunchListTypes.GetLaunchList,
-    GetLaunchListTypes.GetLaunchListVariables
-    >(GET_LAUNCHES);
+    GetLaunchListTypes.GetLaunchListVariables>(GET_LAUNCHES)
 
-  if (loading) return <Loading />;
-  if (error || !data) return <p>ERROR</p>;
+  if (loading) return <Loading />
+  if (error) return <p>ERROR</p>
+  if (!data) return <p>Not found</p>
 
   return (
     <Fragment>
@@ -59,35 +67,8 @@ const Launches: React.FC<LaunchesProps> = () => {
       data.launches.launches.map((launch: any) => (
         <LaunchTile key={launch.id} launch={launch} />
       ))}
-      {data.launches &&
-      data.launches.hasMore && (
-        <Button
-          onClick={() =>
-            fetchMore({
-              variables: {
-                after: data.launches.cursor,
-              },
-              updateQuery: (prev, { fetchMoreResult, ...rest }) => {
-                if (!fetchMoreResult) return prev;
-                return {
-                  ...fetchMoreResult,
-                  launches: {
-                    ...fetchMoreResult.launches,
-                    launches: [
-                      ...prev.launches.launches,
-                      ...fetchMoreResult.launches.launches,
-                    ],
-                  },
-                };
-              },
-            })
-          }
-        >
-          Load More
-        </Button>
-      )}
     </Fragment>
-  );
+  )
 }
 
-export default Launches;
+export default Launches
